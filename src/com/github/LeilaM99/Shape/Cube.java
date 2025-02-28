68d2f9178269111c7b7ea9f86bbd195ddf7a8dd1
Leila Mansouri <l71mansouri@gmail.com>
2025-02-28 10:59:11 +0330

    3d class

--- src/com/github/LeilaM99/Shape/Cube.java
+++ src/com/github/LeilaM99/Shape/Cube.java
@@ -1,5 +1,17 @@
 package com.github.LeilaM99.Shape;
 
-public class Cube {
+public class Cube extends Shape {
+    double side;
 
+    Cube(String color, double side) {
+        super(color);
+        this.side = side;
+    }
+
+
+    @Override
+    public void calculateArea() {
+        area= 6 * side * side;
+    }
 }
+
--- src/com/github/LeilaM99/Shape/Triangle.java
+++ src/com/github/LeilaM99/Shape/Triangle.java
@@ -1,5 +1,19 @@
 package com.github.LeilaM99.Shape;
 
-public class Triangle {
+public class Triangle extends Shape {
+    private double base;
+    private double height;
 
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+
+    }
+
+
+    @Override
+    public void calculateArea() {
+        area= 0.5 * base * height;
+    }
 }
